54e8623f0c74e35e67a8c84b3b1c2b69e1dccb51
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-11 12:55:18 +0800

    Fix cant get backend data

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,11 +3,9 @@ const { STATUSCODE } = require("../constants/index");
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== STATUSCODE.NEGATIVE_ONE) {
+    if (allowedOrigins.indexOf(origin) !== STATUSCODE.NEGATIVE_ONE || !origin) {
       callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    } else callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
   exposedHeaders: ["Access-Control-Allow-Origin"],
